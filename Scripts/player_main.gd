a46dffad0a090632745303d385fb19fb20b6acf1
Alejandro Delati Mik <109637374+AleDelati@users.noreply.github.com>
2024-07-31 21:21:09 -0300

    v0.0.02
    -Mejoras sistema de movimiento

--- Scripts/player_main.gd
+++ Scripts/player_main.gd
@@ -1,46 +1,53 @@
-extends Node3D
+extends RigidBody3D
 
+#Variables
+var lastMovement = "null"
+
+@export var movementCD = 0.5
+var movCD = 0
+
+#Referencias
 var player_AP
-var last_Movement = "null"
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	player_AP = $CharacterBody3D/AnimationPlayer
+	player_AP = $Animations/AnimationPlayer
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	Move_Input()
+	Move_Input(delta)
 
-func Move_Input():
+
+func Move_Input(delta):
 	#Gestiona el input de movimiento
-	if(!player_AP.is_playing()):
-		if(Input.is_action_just_pressed("move_Up")):
-			player_AP.play("move_Forward")
-			last_Movement = "foward"
-		if(Input.is_action_just_pressed("move_Down")):
-			player_AP.play("move_Backwards")
-			last_Movement = "backwards"
+	if(movCD <= 0):
+		if(Input.is_action_just_pressed("move_Forward")):
+			ToggleAV(); player_AP.play("move_Forward"); lastMovement = "foward"; movCD = movementCD
+		if(Input.is_action_just_pressed("move_Backwards")):
+			ToggleAV(); player_AP.play("move_Backwards"); lastMovement = "backwards"; movCD = movementCD
 		if(Input.is_action_just_pressed("move_Right")):
-			player_AP.play("move_Right")
-			last_Movement = "right"
+			ToggleAV(); player_AP.play("move_Right"); lastMovement = "right"; movCD = movementCD
 		if(Input.is_action_just_pressed("move_Left")):
-			player_AP.play("move_Left")
-			last_Movement = "left"
+			ToggleAV(); player_AP.play("move_Left"); lastMovement = "left"; movCD = movementCD
+	
+	#Actualiza el Cooldown de movimiento
+	if(movCD > 0):
+		movCD = movCD - (1 * delta)
 	
 	#Actualiza la posicion del jugador luego de ejecutar las animaciones
-	if(last_Movement != "null"):
+	if(lastMovement != "null"):
 		if(!player_AP.is_playing()):
-			if(last_Movement == "foward"):
-				player_AP.play("idle")
-				position = position + Vector3(0, 0, -1)
-			if(last_Movement == "backwards"):
-				player_AP.play("idle")
-				position = position + Vector3(0, 0, 1)
-			if(last_Movement == "right"):
-				position = position + Vector3(1, 0, 0)
-				player_AP.play("idle")
-			if(last_Movement == "left"):
-				position = position + Vector3(-1, 0, 0)
-				player_AP.play("idle")
-			last_Movement = "null"
+			ToggleAV()
+			if(lastMovement == "foward"): MovePos(Vector3(0, 0, -1))
+			if(lastMovement == "backwards"): MovePos(Vector3(0, 0, 1))
+			if(lastMovement == "right"): MovePos(Vector3(1, 0, 0))
+			if(lastMovement == "left"): MovePos(Vector3(-1, 0, 0))
+			lastMovement = "null"
+
+func ToggleAV(): #Intercambia la visibilidad del Player y el Manager de las animaciones
+	$MeshInstance3D.visible = !$MeshInstance3D.visible
+	$Animations.visible = !$MeshInstance3D.visible
+
+
+func MovePos(pos):
+	position = position + pos
+	player_AP.play("idle")
